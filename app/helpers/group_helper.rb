24b0758c1cd23481341abeeda9e6ef9153d22347
John Wood <john@interactivemediums.com>
2010-04-26 07:44:44 -0500

    Some code cleanup

--- app/helpers/group_helper.rb
+++ app/helpers/group_helper.rb
@@ -1,11 +1,5 @@
-#------------------------------------------------------------------------------#
-# Helper methods for the group views
-#------------------------------------------------------------------------------#
 module GroupHelper
 
-  #----------------------------------------------------------------------------#
-  # Get the list of label types to display in a drop down list
-  #----------------------------------------------------------------------------#
   def get_label_options(label_types)
     label_options = "" 
     label_types.each { |label_type| label_options << "<option>#{label_type}</option>" }
