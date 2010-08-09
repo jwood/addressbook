82a6db5a0422d5e97570c244fece3e98cd5e35bd
John Wood <john@interactivemediums.com>
2010-08-08 21:40:45 -0500

    Some new markup/styles for the mobile version of the app

--- app/helpers/contact_helper.rb
+++ app/helpers/contact_helper.rb
@@ -38,5 +38,13 @@ module ContactHelper
   def display_style_for_specify_address
     (@address.blank? || @address.is_address_empty?) ? "" : "display:none;"
   end
+
+  def has_contact_info_to_display?(contact)
+    !contact.address.ergo.home_phone.ergo.blank? ||
+            !contact.work_phone.blank? ||
+            !contact.cell_phone.blank? ||
+            !contact.email.blank? ||
+            !contact.website.blank?
+  end
   
 end
