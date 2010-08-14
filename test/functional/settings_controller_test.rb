824430bfda4d8c786eaed5ac2368732aee6a893f
John Wood <john@interactivemediums.com>
2010-08-14 13:18:36 -0500

    Added code to get and set the password file

--- test/functional/settings_controller_test.rb
+++ test/functional/settings_controller_test.rb
@@ -22,4 +22,16 @@ class SettingsControllerTest < ActionController::TestCase
     assert_equal "60477", assigns(:address).zip    
   end
 
+  test "should be able to get the password file" do
+    Settings.save_password_file("/home/test/password")
+    xhr :get, :password_file
+    assert_equal "/home/test/password", assigns(:password_file)
+  end
+
+  test "should be able to set the password file" do
+    Settings.save_password_file("/home/test/password")
+    xhr :post, :password_file, :password_file => "/home/smitty/blah"
+    assert_equal "/home/smitty/blah", assigns(:password_file)
+  end
+
 end
