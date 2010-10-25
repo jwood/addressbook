5a8d97be5363740a7511aa7ac2faa56a3e80d359
John Wood <john@interactivemediums.com>
2010-10-24 21:21:18 -0500

    Add http basic auth to the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   has_mobile_fu
 
+  before_filter :authenticate
+
   def use_mobile_view
     session[:mobile_view] = true
     redirect_to root_path
@@ -10,4 +12,18 @@ class ApplicationController < ActionController::Base
     session[:mobile_view] = false
     redirect_to root_path
   end
+
+  private
+
+    def authenticate
+      username = Settings.username
+      password = Settings.password
+
+      if !username.blank? && !password.blank?
+        authenticate_or_request_with_http_basic("Addressbook") do |http_username, http_password|
+          http_username == username && http_password == password
+        end
+      end
+    end
+
 end
