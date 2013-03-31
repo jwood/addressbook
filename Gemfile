1084b3727bccb0af2a28b868e41f92bf6d038413
John Wood <john@johnpwood.net>
2013-03-31 10:13:56 -0500

    Start upgrade to Rails 3.2

--- Gemfile
+++ Gemfile
@@ -1,15 +1,28 @@
-source :gemcutter
+source 'https://rubygems.org'
 
-gem 'rails', '3.0.20'
-gem 'rake', '0.9.2'
-gem 'mysql2', '0.2.18'
-gem 'activerecord-mysql2-adapter', '0.0.3'
-gem 'jquery-rails', '1.0.12'
+gem 'rails', '3.2.13'
+gem 'jquery-rails'
+gem 'mysql2'
 
+gem 'capistrano'
 gem 'pdf-labels', '2.0.1', :require => 'pdf/label'
+gem 'json'
+gem 'dynamic_form'
 
-gem 'rcov'
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+
+  # See https://github.com/sstephenson/execjs#readme for more supported runtimes
+  # gem 'therubyracer', :platforms => :ruby
+
+  gem 'uglifier', '>= 1.0.3'
+end
 
 group :test do
+  gem 'rcov'
   gem 'shoulda'
 end
+
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,19 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
+//= require jquery
+//= require jquery_ujs
+//= require_tree .
+
 dojo.require("dijit.layout.AccordionContainer");
 dojo.require("dijit.layout.ContentPane");
 dojo.require("dijit.layout.BorderContainer");
@@ -95,4 +111,5 @@ function hideSpinner() {
 $(".ajax_request, .button_to").
   live('ajax:before', function() { showSpinner(); }).
   live('ajax:complete', function() { hideSpinner(); });
+
 
