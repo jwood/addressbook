ab8294112e284b19d1b2d8c95ec605d768309276
John Wood <john@johnpwood.net>
2013-03-31 20:06:00 -0500

    Add additional assets to the precompile step

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Addressbook::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( jquery.fancybox-1.3.1.pack.js jquery.fancybox-1.3.1.css mobile.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
