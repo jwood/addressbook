94b81d0de9b52f1670a1515c5ee59b4c01684650
John Wood <john.wood@centro.net>
2013-07-09 09:04:22 -0500

    Pull the secret_key_base value from an environment variable

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Addressbook::Application.config.secret_key_base = ApplicationConfig.secret_token
+Addressbook::Application.config.secret_key_base = ENV['RAILS_SECRET_TOKEN']
