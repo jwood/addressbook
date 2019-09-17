44be52d98ececc94a1647b2547dd0dfce86df6da
Hasan Diwan <hd1@jsc.d8u.us>
2019-09-17 07:19:16 +0200

    make rails serve all files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,8 @@ Addressbook::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  # FIXME
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -38,6 +39,7 @@ Addressbook::Application.configure do
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
+  config.log_level = :info
 
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
