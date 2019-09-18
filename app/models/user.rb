834517d62ec310398b49b39eb27f3089be179d60
Hasan Diwan <hd1@jsc.d8u.us>
2019-09-18 01:01:39 -0700

    make devise confirm users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   has_many :groups
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :confirmable, :registerable,
          :recoverable, :rememberable, :validatable
 end
